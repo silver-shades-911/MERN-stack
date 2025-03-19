a5778e63d602678361e3b155f9b436fec3044996
silver-shades-911 <techtopic1234@gmail.com>
2025-03-19 21:21:59 +0530

    Add more comments to explain consepts

--- Backend/Express.js/cookies_ &_session/cookieServer.js
+++ Backend/Express.js/cookies_ &_session/cookieServer.js
@@ -1,3 +1,12 @@
+/*  
+  ? What Are Cookies?
+    Cookies are small pieces of data stored in the user's browser.
+    They are sent from the server to the client and stored on the client’s machine. 
+    Each subsequent request from the client will include the cookie data, making cookies useful for:
+        - Session management (e.g., login sessions).
+        - Personalization (e.g., user preferences).
+        - Tracking (e.g., analytics and user behavior).
+*/
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
@@ -8,8 +17,8 @@ app.use(cookieParser("secretcode"));
 // cookies 
 // Random route 
 app.get("/random", (req, res) => {
-    res.cookie("username", "@_asim_123");
-   res.send("This is random route");
+    res.cookie("username", "@_asim_123"); // we send cookies in form of Name-value pair , here "username" is name and "@_asim_123" is value
+   res.send("This route is used to send cookies");
 });
 
 app.get("/info", (req, res)=> {
@@ -20,14 +29,17 @@ app.get("/info", (req, res)=> {
 
 // signedCookies
 app.get("/getsignedcookies", (req, res) => {
-    res.cookie("made-in", "India", {signed : true});
-    res.send("This is just a random page use to send signedCookies");
+    res.cookie("made-in", "India", {signed : true}); //* signed cookies are encrypted cookies, their value is encrypted
+    res.send("This page is use to send signedCookies");
 });
 
-// verify
+// Accessing both signed and unsigned cookies
 app.get("/verify", (req, res) => {
- console.log("Unsigned cookies :- ", req.cookies);
- console.log("Signed cookies :- ", req.signedCookies);
+ console.log("Unsigned cookies :- ", req.cookies); // cookies are stored in req.cookies
+ console.log("Signed cookies :- ", req.signedCookies); // signed cookies are stored in req.signedCookies
+
+
+
  res.send("This page get both Signed and Unsigned cookies and print");
 });
 
