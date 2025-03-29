9e6bc286d6962d9f6eb2122ee1e414a08454f6b6
silver-shades-911 <techtopic1234@gmail.com>
2025-03-30 00:17:48 +0530

    Lottery Game is Done

--- Frontend/React/react_lottery_game/src/LotteryMachine.jsx
+++ Frontend/React/react_lottery_game/src/LotteryMachine.jsx
@@ -1,62 +1,65 @@
 import { useState } from "react";
 import Ticket from "./Ticket";
+import { arrayDigitGenerator } from "./helper";
+import Button from "./button";
 
-export default function LotteryMachine() {
-
-  // Lottery Ticket  
-  let [lotteryNo, setLotteryNo ] = useState([0, 0, 0]);
+export default function LotteryMachine({ digitSize = 2, winCondition }) {
+  // Lottery Ticket
+  let [lotteryNo, setLotteryNo] = useState(arrayDigitGenerator(digitSize));
+  console.log("lotteryNo ->", lotteryNo);
 
   // result
   let [result, setResult] = useState("");
 
   // New Ticket Generation Logic
-  function handleNewTicketGenerate () {
-   let NewTicketNo = [];
-   for(let i = 1; i <= 3; i++ ){
-     let digit = Math.floor(Math.random()*10);
-     console.log("digit ->", digit);
-     NewTicketNo.push(digit);
-   }
-   setLotteryNo(NewTicketNo);
-   setResult("");
-  };
-
+  function handleNewTicketGenerate() {
+    setLotteryNo(arrayDigitGenerator(digitSize));
+    setResult("");
+  }
 
   // Lottery ticket Check Logic
-
-  function handleLotteryTicketCheck () {
-    let sum = 0;
-    for(let digit of lotteryNo){
-        sum += digit;
-    };
-    console.log("Sum of ticket digits ->", sum);
-    
-    if(sum == 15){
-        setResult("🎉 Congratulations! You Win the Lottery");
-    }else{
-        setResult("😢 Better Luck Next Time! You lost the Lottery, try again");
+  function handleLotteryTicketCheck() {
+    // here we using winCondition function we pass as props from App component
+    if (winCondition(lotteryNo)) {
+      setResult("🎉 Congratulations! You Win the Lottery");
+    } else {
+      setResult("😢 Better Luck Next Time! You lost the Lottery, try again");
     }
-  };
-
+  }
 
   return (
-    <div style={{ height: "30rem", width: "40rem", border: "2px solid"}}>
+    <div style={{ height: "30rem", width: "40rem", border: "2px solid" }}>
       <h2>Lottery Machine</h2>
-
       <h4>Let's Start Game </h4>
       <br />
       <h4>{result}</h4>
       <br />
       <Ticket lotteryNo={lotteryNo} />
       <br />
-      <span>
-        <button onClick={handleNewTicketGenerate}>Get New Ticket</button>
-      </span>
+      <Button
+        actionCallback={handleNewTicketGenerate}
+        btnName="Get New Ticket"
+      />
       &nbsp; &nbsp; &nbsp; &nbsp;
-      <span>
-        <button onClick={handleLotteryTicketCheck}>Check</button>
-      </span>
-      
+      <Button actionCallback={handleLotteryTicketCheck} btnName="Check" />
     </div>
   );
 }
+
+// Smart Component
+// Propes -> digitSize, winningSum
+// State ->  lotteryNo
+// Events ->  handleNewTicketGenerate, handleLotteryTicketCheck
+
+/*
+
+* FUNCTIONS AS PROPS
+? How we can pass functions as value ?
+  - JS Functions are 1st Class Objects
+  - This means they can be passed to a function as argument, returned from it & assigned to a variable (like normal value)
+  - This is not possible in any programming language  
+  - e.g. 1) winCondition 2) Button onClick callback
+
+* TIP
+  - Think of scalability, Normal user perspective to use & Changability e.g. conditions for win at outer level make easy to change  
+*/
--- Frontend/React/react_lottery_game/src/Ticket.jsx
+++ Frontend/React/react_lottery_game/src/Ticket.jsx
@@ -1,19 +1,31 @@
-
+import TicketNum from "./TicketNum";
 
 export default function Ticket({lotteryNo}) {
 
     let ticketStyle = {
-        // height: "5rem",
+        height: "8rem",
         width: "20rem",
         backgroundColor: "gold", 
-        padding: "1rem",
         border: "2px dashed black ",
-        margin: "0.5rem 9rem"
+        margin: "0.5rem 10rem"
     }
 
     return (
         <div style={ticketStyle}>
-            <p>Your Lottery Number = <b>{lotteryNo}</b></p>
+            <h4 style={{marginBottom: "0"}}>Ticket</h4>
+            {
+                lotteryNo.map(
+                    (digit) => (
+                        <TicketNum num={digit}/>
+                    )
+                )
+            }
         </div>
     );
 };
+
+
+// Dumb Component
+// Propes -> lotteryNo (Array)
+// State -> 0
+// event -> 0
