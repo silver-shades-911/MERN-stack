f124a92fe9a5a7b3e95259368d775478cc07f395
silver-shades-911 <techtopic1234@gmail.com>
2025-03-30 16:22:07 +0530

    New Form concept is added

---
+++ Frontend/React/react_form_app6/src/Form.jsx
@@ -0,0 +1,108 @@
+import { useState } from "react";
+
+function Form() {
+  // style
+  let inputStyle = { backgroundColor: "white", color: "black" };
+
+  // Single common object as state variable, In create each form element have their own state ,so we marge thier individual states with react state
+  let [formInputs, setFormInputs] = useState({
+    userName: "",
+    email: "",
+    password: "",
+  });
+
+  //handleFormInputs  -> This function is use when change in input field's value happens
+
+  function handleInputChange(event) {
+    console.log(`${event.target.name} = ${event.target.value}`);
+    setFormInputs((currInput) => {
+      return {
+        ...currInput,
+        [event.target.name]: event.target.value,
+      };
+    });
+  }
+
+  //handleFormSubmit  -> This function is from form submission
+
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    console.log("Form Input ->", formInputs);
+
+    setFormInputs({
+      userName: "",
+      email: "",
+      password: "",
+    });
+  }
+
+  return (
+    <form action="#">
+      <h2>Form</h2>
+      <br />
+      <br />
+      <label htmlFor="userName">Username:</label>
+      <br />
+      <input
+        type="text"
+        name="userName"
+        id="userName"
+        style={inputStyle}
+        value={formInputs.userName}
+        onChange={handleInputChange}
+      />
+      <br />
+      <br />
+      <label htmlFor="">Email:</label>
+      <br />
+      <input
+        type="email"
+        name="email"
+        id="email"
+        style={inputStyle}
+        value={formInputs.email}
+        onChange={handleInputChange}
+      />
+      <br />
+      <br />
+      <label htmlFor="password">Password:</label>
+      <br />
+      <input
+        type="text"
+        name="password"
+        id="password"
+        style={inputStyle}
+        value={formInputs.password}
+        onChange={handleInputChange}
+      />
+      <br />
+      <br />
+      <button type="submit" onClick={handleFormSubmit}>
+        Submit
+      </button>
+    </form>
+  );
+}
+
+export default Form;
+
+/*
+*  POINTS
+   - Give same name in name attribute of each element as same as their respective key in state variable object 
+     e.g.  <input type="text" name="userName" id="userName" style={inputStyle} onChange={handleFormInputs}/> here name="userName" same as its respective key { userName, ... } 
+   - It help while updating state variable, due to similiar names give access to that key in state variable { }  
+
+*  FLOW
+    
+     input --> onChange --"invoke"--> handleInputChange() --> setFormInputs() --"Update state"--> formInputs [variable's state]  
+    
+      /\                                                                                                | 
+       |                                                                                                |
+       |____________________________________  Value = fromInputs     <__________________________________|
+                                          ( Assigning that updated value
+                                        to input fields, So it visible on UI )
+
+
+     - In this Flow thier is not role of Submit Button , thier flow is different , their role is What to do after submit 
+     - Above Input--value--StateVariable Flow's role is to store Data & Display Data on screen                                   
+*/
