d1cf0a65f9900c08557027f695069d55da892c0e
silver-shades-911 <techtopic1234@gmail.com>
2025-05-03 01:17:43 +0530

    cards created

--- Frontend/React_harry/SuperNews/src/components/NewsItem.jsx
+++ Frontend/React_harry/SuperNews/src/components/NewsItem.jsx
@@ -1,11 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class NewsItem extends Component {
   render() {
+    let {title, description, imageURL, newsURL} = this.props;
     return (
-      <div>
-        This is new item 
-      </div>
-    )
+        <div className="card" style={{width:"18rem",}}>
+          <img src={imageURL} className="card-img-top" alt="..." style={{height: "11rem"}}/>
+          <div className="card-body">
+            <h5 className="card-title">{title.slice(0,20)}</h5>
+            <p className="card-text">
+             {description.slice(0, 90)}
+            </p>
+            <a href={newsURL} className="card-link">Read More</a>
+          </div>
+        </div>
+ 
+    );
   }
 }
