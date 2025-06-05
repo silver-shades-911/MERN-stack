963c6bb126d6c9a985c4213c6c467059f2d8d2d6
silver-shades-911 <techtopic1234@gmail.com>
2025-06-06 01:03:04 +0530

    Project: ToDesktop : Hero section is creadted

---
+++ Frontend/Tailwind_Learning/ToDesktop/src/main.js
@@ -0,0 +1,10 @@
+import './style.css'
+
+let sideNav= document.getElementById("sideNav");
+
+function handleMove () {
+  sideNav.classList.toggle("hidden");
+  console.log
+};
+
+window.handleMove = handleMove ;
