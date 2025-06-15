451e2645764893245b4f911555edd7bb6d3a2b72
silver-shades-911 <techtopic1234@gmail.com>
2025-06-16 01:05:03 +0530

    ToDesktop skill gaining and learning version is Done

--- Frontend/Tailwind_Learning/ToDesktop/src/main.js
+++ Frontend/Tailwind_Learning/ToDesktop/src/main.js
@@ -1,18 +1,22 @@
 import "./style.css";
 
 let sideNav = document.getElementById("sideNav");
+let hamburger = document.getElementById("hamburger");
+let xmark = document.getElementById("xmark");
+
+hamburger.addEventListener("click", () => {
+  sideNav.classList.toggle("hidden");
+});
+
+xmark.addEventListener("click", () => {
+  sideNav.classList.toggle("hidden");
+});
 
 // const value of our lanes initial postions we set
 
 const initialTranslateLTR = -48 * 4;
 const initialTranslateRTL = 36 * 4;
 
-function handleMove() {
-  sideNav.classList.toggle("hidden");
-  console.log;
-}
-
-window.handleMove = handleMove;
 
 function setUpIntersectionObserver(element, isLTR, speed) {
   // intersection callback
@@ -58,23 +62,18 @@ function setUpIntersectionObserver(element, isLTR, speed) {
 setUpIntersectionObserver(lane1, true, 0.15);
 setUpIntersectionObserver(lane2, false, 0.15);
 setUpIntersectionObserver(lane3, true, 0.15);
-setUpIntersectionObserver(lane4, true, 0.7)
-
-
+setUpIntersectionObserver(lane4, true, 0.7);
 
 // FAQs collaps logic
 
-let dtElements = document.querySelectorAll('dt');
+let dtElements = document.querySelectorAll("dt");
 
-dtElements.forEach(
-  (element) => {
-    element.addEventListener("click", () => { 
-      let ddID = element.getAttribute('aria-controls');
-      let ddElement = document.getElementById(ddID);
-      let arrowIcon = element.querySelector('i');
-      ddElement.classList.toggle('hidden');
-      arrowIcon.classList.toggle('-rotate-180');
-
-    });
-  }
-)
+dtElements.forEach((element) => {
+  element.addEventListener("click", () => {
+    let ddID = element.getAttribute("aria-controls");
+    let ddElement = document.getElementById(ddID);
+    let arrowIcon = element.querySelector("i");
+    ddElement.classList.toggle("hidden");
+    arrowIcon.classList.toggle("-rotate-180");
+  });
+});
