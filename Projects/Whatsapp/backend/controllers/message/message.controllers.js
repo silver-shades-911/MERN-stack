4c8ce688448caccc79b093deb1f0fe2e463d895a
red-dragon <techtopic1234@gmail.com>
2025-06-29 00:53:13 +0530

    get messages  route done

--- Projects/Whatsapp/backend/controllers/message/message.controllers.js
+++ Projects/Whatsapp/backend/controllers/message/message.controllers.js
@@ -12,14 +12,14 @@ export const sendMessageController = async (req, res) => {
     const message = req.body.message;
 
     // find conversation exists between these users or not
-    let conversation = await Conversation.find({     // return => conversation => []
+    let conversation = await Conversation.findOne({
       participants: { $all: [senderId, receiverId] },
     });
 
-    console.log("conversation =>", conversation); 
+    console.log("conversation =>", conversation);
 
     // if conversation not exist , they are talking first time
-    if (conversation.length  == 0) {
+    if (!conversation) {
       conversation = await Conversation.create({
         participants: [senderId, receiverId],
       });
@@ -27,32 +27,66 @@ export const sendMessageController = async (req, res) => {
 
     // create newMessage
     const newMessage = await Message.create({
-        senderId,
-        receiverId,
-        message,
+      senderId,
+      receiverId,
+      message,
     });
 
-    // if newMessage is not creats
-    if(newMessage){
+    console.log("newMessage =>", newMessage);
+
+    // if newMessage is creats
+    if (newMessage) {
       await conversation.messages.push(newMessage._id);
-    };
+    }
 
     //TODO: Socket.io functunality come here
-     
-
 
     // * this is lower way , means not run parellaly
     // await newMessage.save(); // if it take 2s
     // await conversation.save();// then this have to wait for 2s
 
     //* parellal executing way
-    await Promise.all[ newMessage.save(), conversation.save() ];
+    await Promise.all[newMessage.save(), conversation.save()];
 
     res.status(200).json({
-        message: newMessage?.message,
-    })
+      message: newMessage?.message,
+    });
   } catch (error) {
     console.log("Error in sendMessageController", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const getMessagesController = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const senderId = req.user._id;
+
+    // check conversation exists
+    const conversation =  await Conversation.findOne({
+      participants: { $all: [senderId, userToChatId] },
+    }).populate("messages");
+
+     
+    console.log("conversation => ",conversation);
+
+    // if not exists
+    if (!conversation) {
+      return res.status(404).json({ error: "Conversation not exists" });
+    }
+
+    // if exists then access messages
+    const messages = conversation?.messages;
+
+    // if messages not exists
+    if (messages.length == 0) {
+      return res.status(404).json({ error: "No Messeages" });
+    }
+
+    // if messages exists then send to res
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("Error in getMessagesController", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
--- Projects/Whatsapp/backend/routes/message.routes.js
+++ Projects/Whatsapp/backend/routes/message.routes.js
@@ -1,17 +1,20 @@
 // packages import
 import express from "express";
 
-
 // modules import
-import { sendMessageController } from "../controllers/message/message.controllers.js";
+import {
+  sendMessageController,
+  getMessagesController,
+} from "../controllers/message/message.controllers.js";
 import { protectRoute } from "../middlewares/protectRoute.js";
-
 
 // varibales
 const router = express.Router();
 
 // ROUTE 1 => Create a Message using : POST "/api/message/send/:id". Login Required
-router.post("/send/:id", protectRoute ,sendMessageController);
+router.post("/send/:id", protectRoute, sendMessageController);
 
+// ROUTE 2 => Get Messages using GET "/api/messages/:id" . login Required
+router.get("/:id", protectRoute, getMessagesController);
 
 export default router;
